6ad67439535710228d301ab949dc797aea0c23d4
Marcos Madala <marcosmadala@gmail.com>
2025-10-09 22:39:55 -0300

    Update checkbox.tsx

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -11,8 +11,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-      "border-[var(--checkbox-color,theme(colors.primary))] data-[state=checked]:bg-[var(--checkbox-color,theme(colors.primary))] data-[state=checked]:text-white data-[state=checked]:border-[var(--checkbox-color,theme(colors.primary))]",
+      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className
     )}
     {...props}
